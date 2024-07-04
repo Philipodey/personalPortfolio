708e7ff73c1083d1127cc9bfd9e37f84eea4ed1f
Philip Odey <philipodey75@gmail.com>
2024-07-04 17:15:32 +0100

    commit push

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import styles from './App.module.css'
+import {Navbar} from "./component/Navbar/Navbar.jsx";
+import {Hero} from "./component/Hero/Hero.jsx";
+import {About} from "./component/About/About.jsx";
+import {Experience} from "./component/Experience/Experience.jsx";
+import {Projects} from "./component/Projects/Projects.jsx";
+
+function App() {
+
+  return (
+    <div className={styles.App}>
+      <Navbar/>
+        <Hero/>
+        <About/>
+      <Experience/>
+        <Projects/>
+    </div>
+  )
+}
+
+export default App
