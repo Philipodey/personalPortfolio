d43c29f7c76fbad44689247ef16e524eb2d43471
Philip Odey <philipodey75@gmail.com>
2024-07-05 03:41:33 +0100

    another commit

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -1,9 +1,38 @@
-
+import projects from "../../data/projects.json"
+import {getImageUrl} from "../../utils.js";
 
 export const Projects=()=>{
     return(
         <div>
-            Projects
+            <h2>
+                Projects
+            </h2>
+            <div>
+                {
+                    projects.map((project, id)=>{
+                        return(
+                            <div key={id}>
+                                <img src={getImageUrl(project.imageSrc)}
+                                     alt={`image of ${project.imageSrc}`}
+                                />
+                                <h3>{project.title}</h3>
+                                <p>{project.description}</p>
+                                <ul>
+                                    {
+                                        project.skills.map((skill, id)=>{
+                                        <li key={id}>{skill}</li>
+                                        })
+                                    }
+                                </ul>
+                                <div>
+                                    <a href={project.demo}>Demo</a>
+                                    <a href={project.source}>Source</a>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
